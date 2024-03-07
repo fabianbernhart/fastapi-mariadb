5109f1f6952b70b722e1a4b641478bfecfe28865
Bradley Hower <139923955+Bradley-Hower@users.noreply.github.com>
2024-03-07 15:32:53 -0800

    Update database.py
    Added ability to use .env file for environment variables.

--- src/db/database.py
+++ src/db/database.py
@@ -1,13 +1,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from . import config
 
-SQLALCHEMY_DATABASE_URL = "mariadb+pymysql://root:toor@127.0.0.1:3306/company"
+DATABASE_USERNAME = config.DATABASE_USERNAME
+DATABASE_PASSWORD = config.DATABASE_PASSWORD
+DATABASE_HOST = config.DATABASE_HOST
+DATABASE_NAME = config.DATABASE_NAME
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}"
+
+# SQLALCHEMY_DATABASE_URL = "mariadb+pymysql://root:toor@127.0.0.1:3306/company"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
-engine = create_engine(
-    url=SQLALCHEMY_DATABASE_URL
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
